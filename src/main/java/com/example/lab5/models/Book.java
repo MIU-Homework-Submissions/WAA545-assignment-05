3496a041bf911d9d41fec2400031b106e256142f
Merhawi Fissehaye <merhawifissehaye@gmail.com>
2023-10-06 15:27:04 -0500

    product inheritance with single table strategy

---
+++ src/main/java/com/example/lab5/models/Book.java
@@ -0,0 +1,8 @@
+package com.example.lab5.models;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Book extends Product {
+    private String title;
+}
---
+++ src/main/java/com/example/lab5/models/CD.java
@@ -0,0 +1,8 @@
+package com.example.lab5.models;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class CD extends Product {
+    private String artist;
+}
