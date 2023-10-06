c75ce9ed36080ff96d0ee67e61f0c492f05a64cb
Merhawi Fissehaye <merhawifissehaye@gmail.com>
2023-10-06 15:29:36 -0500

    switching to joined table strategy

--- src/main/java/com/example/lab5/models/Product.java
+++ src/main/java/com/example/lab5/models/Product.java
@@ -2,10 +2,13 @@ package com.example.lab5.models;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
 import lombok.Data;
 
 @Data
 @Entity
+@Inheritance(strategy = InheritanceType.JOINED)
 public class Product {
     @Id
     private int id;
